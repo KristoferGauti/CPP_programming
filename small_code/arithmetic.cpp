c10f2c8af9417a4716a65d335c5961a81875e35e
kristofer <kristofergauti@gmail.com>
2021-05-23 23:26:55 +0000

    arithmetic while loop solution implemented

--- small_code/arithmetic.cpp
+++ small_code/arithmetic.cpp
@@ -4,12 +4,17 @@ using namespace std;
 int main() {
     int num1;
     int num2;
-    cout << "Adding two numbers together" << endl;
-    cout << "Enter a number: ";
-    cin >> num1;
-    cout << "Enter another number: ";
-    cin >> num2;
-    int answer = num1 + num2;
-    cout << num1 << " + " << num2 << " = " << answer << endl;
+    char try_again = 'y'; //char must be declared with single quotes
+    while (try_again != 'n') {
+        cout << "Adding two numbers together" << endl; //strings must be declared with double quotes
+        cout << "Enter a number: ";
+        cin >> num1;
+        cout << "Enter another number: ";
+        cin >> num2;
+        int answer = num1 + num2;
+        cout << num1 << " + " << num2 << " = " << answer << endl;
+        cout << "Would you like to try again (y/n)? ";
+        cin >> try_again;
+    }
     return 0;
 }
